9e2f684df312a8a0322a24b8d05425d1501aba05
shantanujo <shantanuvanellus@gmail.com>
2023-03-27 09:26:27 -0500

    updated slurm script

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python 3
 
-job_name = "job_assn04"
-queue_name= "comp007"
-num_nodes= "1"
-num_processors= "32"
-walltime= "0"
+job_name = 'job_assn04'
+queue = 'comp01'
+num_nodes = 1
+num_processors = 24
+walltime = 1
 
-print('#SBATCH --job-name=',job_name)
-print('#SBATCH --partition', queue_name)
-print('#SBATCH --nodes=',num_nodes)
+#print bash header
+
+print('#SBATCH --job-name=' + job_name)
+print('#SBATCH --partition' + queue)
+print('#SBATCH --nodes=' + str(num_nodes))
 print('#SBATCH --qos comp')
-print('#SBATCH --tasks-per-node=',num_processors)
-print('#SBATCH --time=',walltime)
+print('#SBATCH --tasks-per-node=' + str(num_processors))
+print('#SBATCH --time=' + str(walltime) + ':00:00')
 print('#SBATCH -o test_%j.out')
 print('#SBATCH -e test_%j.err')
 print('#SBATCH --mail-type=ALL')
 print('#SBATCH --mail-user=sj058@uark.edu')
 
-cd $SLURM_SUBMIT_DIR
+#cd $SLURM_SUBMIT_DIR
 
 # job command here
 #command 1
