6f4a216659b87731f4f96af829fc33c033574850
shantanujo <shantanuvanellus@gmail.com>
2023-03-29 11:28:38 -0500

    added the new slurm script with parsed variables

---
+++ new_slurm_jo.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import argparse
+parser= argparse.ArgumentParser("Running a job on AHPCC with variables")
+
+# adding opsitional argument
+parser.add_argument("job_name", help='specifies the job name',type=str)
+
+#adding optional args
+parser.add_argument("-q","--queue", help='specifies the queue', type=str)
+parser.add_argument("-n", "--nodes", help='specifies the number of nodes', type=str)
+parser.add_argument("-np","--num_processors", help='specifies the number of prcessors to use', type=str)
+parser.add_argument("-w","--walltime", help='specifies the walltime', type=str)
+
+args= parser.parse_args()
+
+#print variables
+print('#SBATCH --job-name=' + args.job_name)
+print('#SBATCH --partition' + str(args.queue))
+print('#SBATCH --nodes=' + str(args.nodes))
+print('#SBATCH --qos comp')
+print('#SBATCH --tasks-per-node=' + str(args.num_processors))
+print('#SBATCH --time=' + str(args.walltime) + ':00:00')
+print('#SBATCH -o test_%j.out')
+print('#SBATCH -e test_%j.err')
+print('#SBATCH --mail-type=ALL')
+print('#SBATCH --mail-user=sj058@uark.edu')
+
+#cd $SLURM_SUBMIT_DIR
+
+
+
