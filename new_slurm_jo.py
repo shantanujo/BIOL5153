0b7a886f65a2e20f3161cb74a478d3f11ef7d7b3
shantanujo <shantanuvanellus@gmail.com>
2023-04-12 09:44:01 -0500

    adding the parser scripts edited in class

--- new_slurm_jo.py
+++ new_slurm_jo.py
@@ -8,12 +8,14 @@ parser.add_argument("job_name", help='specifies the job name',type=str)
 
 #adding optional args
 parser.add_argument("-q","--queue", help='specifies the queue', type=str, default= 'comp01')
-parser.add_argument("-n", "--nodes", help='specifies the number of nodes', type=str, default='1')
-parser.add_argument("-np","--num_processors", help='specifies the number of prcessors to use', type=str, default='32')
-parser.add_argument("-w","--walltime", help='specifies the walltime', type=str)
+parser.add_argument("-n", "--nodes", help='specifies the number of nodes', type=int, default='1')
+parser.add_argument("-p","--num_processors", help='specifies the number of prcessors to use', type=int, default='24')
+parser.add_argument("-w","--walltime", help='specifies the walltime', type=int, default='1')
 
 args= parser.parse_args()
 
+print('#!/bin/bash')
+print()
 #print variables
 print('#SBATCH --job-name=' + args.job_name)
 print('#SBATCH --partition', str(args.queue))
@@ -25,5 +27,10 @@ print('#SBATCH -o test_%j.out')
 print('#SBATCH -e test_%j.err')
 print('#SBATCH --mail-type=ALL')
 print('#SBATCH --mail-user=sj058@uark.edu')
-#cd $SLURM_SUBMIT_DIR
+print()
+print('module purge')
+print()
+print('cd $SLURM_SUBMIT_DIR')
+
+#job commands here
 
--- parser_gff.py
+++ parser_gff.py
@@ -1,37 +1,33 @@
 #! /usr/bin/env python3
 import argparse
-
+import csv
 
 parser= argparse.ArgumentParser("reading a GFF file")
-
 # adding positional argument
 parser.add_argument("gff", help='opens specified gff file',type=str)
 parser.add_argument("fasta", help= 'opens FASTA file', type=str)
 
 ## optional arguments
-
 args= parser.parse_args()
 
 #open files
 gff_file = open(args.gff, "r")
 fasta_file = open(args.fasta, "r")
 
-#read the files
-#with open(args.gff) as x:
-	#for line in x:
-		#print(line.rstrip())
-
-with open(args.fasta) as y:
+with open (args.fasta) as y:
 	for line in y:
 		print(line.rstrip())
 line = 0
-lent= 0
 #feature type, length
 with open(args.gff) as x:
-		for line in x:
-			line = line.rstrip()
-			line = line.split('\t')
-			lent = int(line[4]) - int(line[3])
-			lent += 1
-			feat_name = line[2]
-			print(feat_name, lent)
+	reader = csv.reader(x, delimiter='\t')
+	for line in reader:
+		if not line:
+			continue
+		else:
+			start = int(line[3])
+			end = int(line[4])
+			lent = end - start + 1
+			print(line[2], lent)
+			line[5] = str(lent)
+			new_line = "\t".join(line)
