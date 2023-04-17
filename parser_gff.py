b2dbdab6049812ab6c60c68b6535a5b86b02a423
shantanujo <shantanuvanellus@gmail.com>
2023-04-17 09:44:18 -0500

    parser script updated with fasta file code

--- parser_gff.py
+++ parser_gff.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import argparse
 import csv
+from Bio import SeqIO
 
 parser= argparse.ArgumentParser("reading a GFF file")
 # adding positional argument
@@ -10,6 +11,10 @@ parser.add_argument("fasta", help= 'opens FASTA file', type=str)
 ## optional arguments
 args= parser.parse_args()
 
+
+#FASTA file
+genome = SeqIO.read("covid.fasta", "fasta")
+
 #open files
 gff_file = open(args.gff, "r")
 fasta_file = open(args.fasta, "r")
@@ -18,6 +23,7 @@ with open (args.fasta) as y:
 	for line in y:
 		print(line.rstrip())
 line = 0
+
 #feature type, length
 with open(args.gff) as x:
 	reader = csv.reader(x, delimiter='\t')
@@ -31,3 +37,10 @@ with open(args.gff) as x:
 			print(line[2], lent)
 			line[5] = str(lent)
 			new_line = "\t".join(line)
+			feat_seq = genome.seq[start-1: end]
+			organism = line[0]
+			source = line[1]
+			attri = line[8]
+			feat_type = line[2]
+			print(">" + organism, feat_type, attri, feat_seq)
+			print(feat_seq)
